fe92e69f5b7128d38be0d58042b660d58c37288a
stoba1400 <stob419@gmail.com>
2021-07-14 10:22:55 -0400

    Change to React

---
+++ src/projectData.js
@@ -0,0 +1,30 @@
+export const projects = [
+    {
+      title: "NBA Player Selector",
+      subtitle: "React, TypeScript, Firebase",
+      description:
+        "• Selection of 450+ NBA players used in state value from Firestore database collection to create teams\nLogin authorization for users with current profile passed down using React Context API\n• Swiping feature with players featured from Firestore database",
+      link: "https://github.com/stoba1400",
+    },
+    {
+      title: "Checkout Bot",
+      subtitle: "Python, HTML, Selenium framework",
+      description:
+        "• Created a purchasing bot for an individual unable to purchase items online\n• Identified elements through HTML analysis and used Selenium to auto-fill checkout information and purchase through personal account",
+      link: "https://github.com/stoba1400",
+    },
+    {
+      title: "Personal Website",
+      subtitle: "React, TypeScript",
+      description:
+        "• This website here! Captures important career information and styled with Tailwind CSS.",
+      link: "https://github.com/stoba1400",
+    },
+    {
+      title: "Sudoku",
+      subtitle: "C++, Docker, Catch2 framework",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Praesentium dolore rerum laborum iure enim sint nemo omnis voluptate exercitationem eius?",
+      link: "https://pythonbootcamp.com",
+    },
+  ];
---
+++ src/workData.js
@@ -0,0 +1,23 @@
+export const work = [
+    {
+      company: "PricewaterhouseCoopers",
+      title: "Tax Technology Developer Intern",
+      description:
+        "• Utilize Software Development Life Cycle\n• Building models and data visualization methods\n• Work under Agile methodology while understanding C# OOP practices\n• Creating strong testing processes using JMeter and BlazeMeter",
+      link: "https://pwc.com",
+    },
+    {
+      company: "First Solar",
+      title: "Manufacturing Systems Intern",
+      description:
+        "• Synchronize databases across six production plants\n• Led development of database alignment application in C# with SQL Server connections.\n• Visualized relational database data using Tableau",
+      link: "https://firstsolar.com",
+    },
+    {
+      company: "Otsego Local Schools",
+      title: "Accounting Intern",
+      description:
+        "• Efficiently use Excel and Quickbooks\n• Record and post transactions, accounts payable and receivable, and personal invoices for school district",
+      link: "https://jsbootcamp.com",
+    },
+  ];
