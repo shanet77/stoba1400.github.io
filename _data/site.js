cc66a86d40e39e9c85d89bf5d16eb10f46275080
Shane <stob419@gmail.com>
2025-11-23 18:15:30 -0600

    Fix leftover files

--- _data/site.js
+++
@@ -1,6 +0,0 @@
-export default {
-    title: "Shane Tobar",
-    description: "A personal blog and resume site.",
-    author: "Shane Tobar",
-    baseUrl: "" // Leave empty for relative URLs, or set to your domain (e.g., "https://yourdomain.com")
-};
--- blog/first-post.md
+++
@@ -1,7 +0,0 @@
----
-title: Let's Go 'Cers
-date: 2025-06-15
-layout: layouts/post.njk
----
-
-SGA, FTA, VGA, doesn't matter. Pacers in six.
